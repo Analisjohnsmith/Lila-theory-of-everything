82d86025d330ba52fc98c4a5dcff873b12a881ba
Darrell lee Stiltner <leelastiltner@gmail.com>
2026-07-23 03:09:19 -0700

    Update description of the app in BLOB file

--- Continuum Problem Space  BLOB .cc
+++ Continuum Problem Space  BLOB .cc
@@ -1,4 +1,5 @@
-this is finshed  i'm turning into a app in it's own repo 7/2/26 
+this is a app  it's  the space of all structural problems and the
+the space of all structural problems and solutions. 7/2/26 
 # =============================================================================
 #  Copyright (C) 2026  Darrell Lee (Līlā) Stiltner
 #
